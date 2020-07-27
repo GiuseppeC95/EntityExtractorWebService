55d2ea5e5f421fd2aa2908a928a15b2cf7f6a82f
GiuseppeC95 <s217886@studenti.polito.it>
2020-07-27 15:32:36 +0200

    EDIT Scrollbar style for videolessons

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,22 @@
 $(document).ready(function () {
 
-    //TODO: Controllare meglio questa custom toolbar (può essere interessante per elencare le videolezioni)
-    // $('#sidebar').mCustomScrollbar({
-    //      theme: "minimal"
-    // });
+    // TODO: Controllare meglio questa custom toolbar (può essere interessante per elencare le videolezioni)
+    $(function() {
+        $('.defaultScrollStartup').removeClass("defaultScrollStartup")
+        //The passed argument has to be at least a empty object or a object with your desired options
+        $('.customScroll').overlayScrollbars({
+
+            className       : "os-theme-dark",
+            overflowBehavior: {
+                x: "scroll",
+                y: "scroll"
+            },
+            scrollbars : {
+                clickScrolling : true,
+                touchSupport: true
+            }
+        });
+    });
 
     $('#sidebarCollapse').on('click', function () {
         $('#sidebar').toggleClass('active');
